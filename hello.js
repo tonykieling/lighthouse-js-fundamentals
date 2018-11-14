dd10499a53a8cca537fbd4c86e38bc121f3bd3ff
Tony Kieling <tony.kieling@gmail.com>
2018-11-14 19:11:45 +0000

    hello.js exercise with 'sayhello' function completed

---
+++ hello.js
@@ -0,0 +1,9 @@
+function sayHello(name = "unknown name") {
+  console.log("Hello, " + name);
+}
+
+sayHello("fulano");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+sayHello();
