3a76c53cb948dca588ad3c09f127cc5975c88804
Tony Kieling <tony.kieling@gmail.com>
2018-11-14 19:27:02 +0000

    even.js exercise with 'isEven' function completed

---
+++ even.js
@@ -0,0 +1,13 @@
+function isEven(num){
+  return num % 2 === 0;
+}
+
+// var tenIsEven = isEven(10);
+// var oneIsEven = isEven(1);
+
+// console.log(tenIsEven);
+// console.log(oneIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
+
