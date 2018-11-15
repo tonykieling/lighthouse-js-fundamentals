452caaa8cdabfa60facda04b9d3e612bb9d8d406
Tony Kieling <tony.kieling@gmail.com>
2018-11-15 17:08:23 +0000

    Adds looping exercises

---
+++ chorus-for.js
@@ -0,0 +1,6 @@
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++){
+  console.log((repeat + 1) + "- " + chorus);
+}
+
+console.log("Until the sun comes up!");
---
+++ packing-list.js
@@ -0,0 +1,10 @@
+var packingList = ["bowls", "plates", "pots", "pans",
+  "eating utensils", "glasses", "cups",
+  "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (var i = 0; i < packingList.length; i++){
+  console.log(packingList[i]);
+}
+
+// console.log("\n\n" + packingList[3]);
