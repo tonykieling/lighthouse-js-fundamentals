5753bc41fac232c002718756aac5a708294b4cab
Tony Kieling <tony.kieling@gmail.com>
2018-11-15 00:36:26 +0000

    Adds conditional exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var temperature = 5;
+
+if (temperature < 5){
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15 && temperature >= 5){
+  console.log("Short sleeves won't cut it.")
+} else {
+  console.log("Short sleeves are fine.")
+}
+
+console.log("Now you're ready to go outside!")
